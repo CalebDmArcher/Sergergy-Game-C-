721f609d40e7408bae262b9093ceafb0aa9fa009
CalebDmArcher <53223583+CalebDmArcher@users.noreply.github.com>
2019-07-23 11:53:33 -0400

    Add files via upload

---
+++ images/failed.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.7
+ * Invocation command was nin10kit --mode=3 --resize=240x160 failed failed.jpg 
+ * Time-stamp: Tuesday 04/09/2019, 01:29:28
+ * 
+ * Image Information
+ * -----------------
+ * failed.jpg 240@160
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef FAILED_H
+#define FAILED_H
+
+extern const unsigned short failed[38400];
+#define FAILED_SIZE 76800
+#define FAILED_LENGTH 38400
+#define FAILED_WIDTH 240
+#define FAILED_HEIGHT 160
+
+#endif
+
